38e0468c672d663dd199117519d24312ef27dca0
Guillermo Lozano <guillermo.lozanob.ext@inetum.com>
2025-02-12 13:05:41 +0100

    Primer test saludar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,12 @@
 package org.example;
 
 public class Main {
+
+
+    public void saludar() {
+        System.out.println("Hola mundo");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
---
+++ src/test/java/MiTest.java
@@ -0,0 +1,12 @@
+import org.example.Main;
+import org.junit.jupiter.api.Test;
+
+
+public class MiTest {
+
+    @Test
+    public void testSaludar() {
+        Main main = new Main();
+        main.saludar();
+    }
+}
