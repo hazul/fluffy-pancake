7452039c81c24c858ca9924e8519f4bb3d73994a
Guillermo Lozano <guillermo.lozanob.ext@inetum.com>
2025-02-12 13:46:57 +0100

    Nuevo test sumar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,10 @@ public class Main {
         System.out.println("Hola mundo");
     }
 
+    public int sumar(int sumando1, int sumando2) {
+        return sumando1 + sumando2;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
--- src/test/java/MiTest.java
+++ src/test/java/MiTest.java
@@ -1,4 +1,5 @@
 import org.example.Main;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
@@ -8,5 +9,10 @@ public class MiTest {
     public void testSaludar() {
         Main main = new Main();
         main.saludar();
+    }
+
+    @Test
+    public void testSuma() {
+        Assertions.assertEquals(7, new Main().sumar(3, 4));
     }
 }
